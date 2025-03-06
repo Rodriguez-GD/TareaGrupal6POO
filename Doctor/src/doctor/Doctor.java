77cb93baaaf3f682202bc0e03ad5a4fdf15a4749
omarl <omarl@DESKTOP-PQFKCT1>
2025-03-06 15:45:57 -0600

    Subclase Doctor creada por Omar Torres

---
+++ Doctor/src/doctor/Doctor.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package doctor;
+
+/**
+ *
+ * @author omarl
+ */
+public class Doctor extends Persona {
+    private String especialidad;
+
+    public Doctor(String nombre, int edad, String especialidad) {
+        super(nombre, edad);
+        this.especialidad = especialidad;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    
+    public void mostrarProfesion() {
+        System.out.println("Soy un doctor especializado en " + especialidad);
+    }
+}
