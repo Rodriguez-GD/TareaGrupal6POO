6312f5b7cbc111c0b41f1a67c956eece5472ae7f
Rodriguez-GD <gdrodriguez1352@gmail.com>
2025-03-05 20:35:26 -0600

    Agregando subclase "Doctor" a Proyecto

---
+++ Master_branch/src/Doctor.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Rodriguez
+ */
+// Subclase Doctor
+public class Doctor extends Persona {
+    private String especialidad;
+
+    public Doctor(String nombre, int edad, String especialidad) {
+        super(nombre, edad);
+        this.especialidad = especialidad;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+  
+    public void mostrarProfesion() {
+        System.out.println("Soy un doctor especializado en " + especialidad);
+    }
+}
